d58745a5b57a8e24d5dc6d41009afa98134f8d3e
Savelying <savely.ing@mail.ru>
2025-02-25 17:20:44 +0500

    Доработана функция списания бонусных баллов

--- src/savelying/Management.java
+++ src/savelying/Management.java
@@ -13,28 +13,29 @@ import java.util.Scanner;
 public class Management {
     final private Scanner reader = new Scanner(System.in);
     String dbName = "Fitness_Center";
-    Frame frame = new Frame(600, 300);
+    Frame frame = new Frame(600, 500);
     Container container = new Container();
+    JPanel panel = new JPanel();
 
     private void printClubOptions() {
         System.out.print("""
                 
                 Список клубов:
+                0) Мультиклубный
                 1) Клуб Меркурий
                 2) Клуб Нептун
                 3) Клуб Юпитер
-                4) Мультиклубный
                 Выберите нужный клуб:\s""");
     }
 
     private int getIntInput() {
-        int choice = 0;
+        int choice = -5;
 
-        while (choice == 0) {
+        while (choice == -5) {
             try {
                 choice = reader.nextInt();
 
-                if (choice == 0)
+                if (choice == -5)
                     throw new InputMismatchException();
                 reader.nextLine();
 
@@ -50,28 +51,28 @@ public class Management {
         container.removeAll();
         container.setLayout(new GridLayout(5, 1));
 
-        JPanel panelChoice = new JPanel();
-        panelChoice.setLayout(new GridLayout(2, 2));
+        panel.removeAll();
+        panel.setLayout(new GridLayout(2, 2));
 
         JButton buttonAdd = new JButton("Добавить члена");
         buttonAdd.addActionListener(e -> addMember(dbName));
-        panelChoice.add(buttonAdd);
+        panel.add(buttonAdd);
         JButton buttonInfo = new JButton("Узнать данные члена");
         buttonInfo.addActionListener(e -> printMemberInfo(dbName));
-        panelChoice.add(buttonInfo);
+        panel.add(buttonInfo);
         JButton buttonBonus = new JButton("Списать бонусы члена");
         buttonBonus.addActionListener(e -> updateMemberPoints(dbName));
-        panelChoice.add(buttonBonus);
+        panel.add(buttonBonus);
         JButton buttonDel = new JButton("Удалить члена");
         buttonDel.addActionListener(e -> removeMember(dbName));
-        panelChoice.add(buttonDel);
+        panel.add(buttonDel);
 
         JButton buttonExit = new JButton("ВЫХОД");
         buttonExit.addActionListener(e -> System.exit(0));
 
         container.add(new Item());
         container.add(new JLabel("ВЫБЕРИТЕ НУЖНОЕ ДЙСТВИЕ", SwingConstants.CENTER));
-        container.add(panelChoice);
+        container.add(panel);
         container.add(new JLabel("", SwingConstants.CENTER));
         container.add(buttonExit);
         frame.add(container);
@@ -96,13 +97,13 @@ public class Management {
         printClubOptions();
         int clubId = getIntInput();
 
-        while (clubId < 1 || clubId > 4) {
+        while (clubId < 0 || clubId > 3) {
             System.out.print("\nОШИБКА: НЕПРАВИЛЬНЫЙ ВВОД. Попробуйте ещё раз:" + " ");
             clubId = getIntInput();
         }
 
         //Вносим клиента в список с соответствующим клубом и тарифом
-        if (clubId != 4) {
+        if (clubId != 0) {
             calculator = (n) -> {
                 return switch (n) {
                     case 1 -> 900;
@@ -117,7 +118,7 @@ public class Management {
         } else {
             calculator = (n) -> {
                 return switch (n) {
-                    case 4 -> 1500;
+                    case 0 -> 1500;
                     default -> -1;
                 };
             };
@@ -184,15 +185,18 @@ public class Management {
         container.removeAll();
         container.setLayout(new GridLayout(5, 1));
 
-        JPanel panelInfo = new JPanel();
-        panelInfo.add(new JLabel("Идентификационный номер:"));
+        panel.removeAll();
+        panel.setLayout(new FlowLayout());
+
+        panel.add(new JLabel("Идентификационный номер:"));
         JTextField idField = new JTextField("", 30);
-        panelInfo.add(idField);
+        idField.setFocusCycleRoot(true);
+        panel.add(idField);
         JButton buttonInfo = new JButton("ПОКАЗАТЬ");
         buttonInfo.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                int id = 0;
+                int id;
                 try {
                     id = Integer.parseInt(idField.getText());
 
@@ -205,38 +209,41 @@ public class Management {
                         if (resultSet.next()) {
                             int points;
 
-                            if (resultSet.getInt("clubid") == 4) {
+                            if (resultSet.getInt("clubid") == 0) {
                                 LocalDate date = LocalDate.now();
                                 Period period = Period.between(resultSet.getDate("date").toLocalDate(), date);
                                 points = (int) period.toTotalMonths() * 100 - resultSet.getInt("points");
                             } else points = resultSet.getInt("points");
 
-                            JOptionPane.showMessageDialog(null, "Имя члена: " + resultSet.getString("name") + "\nТип членства: " + resultSet.getString("type") + "\nНомер клуба: " + resultSet.getInt("clubid") + "\nЧленский взнос: " + resultSet.getInt("fees") + "\nБонусные баллы = " + points, "Информация члена ID №" + resultSet.getInt("id"), JOptionPane.INFORMATION_MESSAGE);
+                            JOptionPane.showMessageDialog(null, "Имя члена: " + resultSet.getString("name") + "\nТип членства: " + resultSet.getString("type") + "\nНомер клуба: " + resultSet.getInt("clubid") + "\nЧленский взнос: " + resultSet.getInt("fees") + "\nБонусные баллы: " + points, "Информация члена ID №" + resultSet.getInt("id"), JOptionPane.INFORMATION_MESSAGE);
 
-                        } else JOptionPane.showMessageDialog(null, "ОШИБКА: Члена с указанным номером нет в списках!", "Информация члена ID №" + id, JOptionPane.ERROR_MESSAGE);
+                        } else
+                            JOptionPane.showMessageDialog(null, "ОШИБКА: Члена с указанным номером нет в списках!", "Информация члена ID №" + id, JOptionPane.ERROR_MESSAGE);
 
                     } catch (SQLException | ClassNotFoundException f) {
                         System.out.println(f.getMessage());
+                        JOptionPane.showMessageDialog(null, "ОШИБКА: Нет связи с базой данных!", "Информация члена ID №" + id, JOptionPane.ERROR_MESSAGE);
                     }
 
                 } catch (Exception m) {
-                    JOptionPane.showMessageDialog(null, "ОШИБКА: Введено не числовое значение!", "Информация члена ID №xx", JOptionPane.ERROR_MESSAGE);
-                };
+                    System.out.println(m.getMessage());
+                    JOptionPane.showMessageDialog(null, "ОШИБКА: Введено не числовое значение!", "Информация члена ID №", JOptionPane.ERROR_MESSAGE);
+                }
+                ;
             }
         });
 
-        panelInfo.add(buttonInfo);
+        panel.add(buttonInfo);
         JButton buttonBack = new JButton("НАЗАД");
         buttonBack.addActionListener(e -> getChoice());
 
         container.add(new Item());
-        container.add(new JLabel("ВВЕДИТЕ НОМЕР ЧЛЕНА", SwingConstants.CENTER));
-        container.add(panelInfo);
+        container.add(new JLabel("ПОКАЗАТЬ ДАННЫЕ ЧЛЕНА №", SwingConstants.CENTER));
+        container.add(panel);
         container.add(new JLabel("", SwingConstants.CENTER));
         container.add(buttonBack);
 
 
-//        frame.removeAll();
         frame.add(container);
         frame.repaint();
         frame.revalidate();
@@ -244,43 +251,88 @@ public class Management {
 
     //Метод расчёта бонусов клиента
     public void updateMemberPoints(String dbName) {
-        String readSQL = "select clubid, date, points from Members where id = ?";
+        String readSQL = "select * from Members where id = ?";
         String writeSQL = "update Members set points = ? where id = ?";
 
-        System.out.print("\nВведите номер члена: ");
-        int id = getIntInput();
+        container.removeAll();
+        container.setLayout(new GridLayout(5, 1));
 
-        try (Connection connection = DBConnector.getDbConnect(dbName)) {
-            PreparedStatement readStatement = connection.prepareStatement(readSQL);
+        panel.removeAll();
+        panel.setLayout(new FlowLayout());
 
-            readStatement.setInt(1, id);
-            ResultSet resultSet = readStatement.executeQuery();
+        panel.add(new JLabel("Идентификационный номер:"));
+        JTextField idField = new JTextField("", 30);
+        panel.add(idField);
+        panel.add(new JLabel("Сколько списать баллов:"));
+        JTextField bonusField = new JTextField("", 30);
+        panel.add(bonusField);
+        JButton buttonBonus = new JButton("СПИСАТЬ");
+        buttonBonus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int id, bonus;
+                try {
+                    id = Integer.parseInt(idField.getText());
+                    bonus = Integer.parseInt(bonusField.getText());
 
-            if (resultSet.next()) {
-                if (resultSet.getInt("clubid") == 4) {
-                    System.out.print("\nВведите количество списываемых бонусов: ");
-                    int points = getIntInput();
+                    try (Connection connection = DBConnector.getDbConnect(dbName)) {
+                        PreparedStatement readStatement = connection.prepareStatement(readSQL);
 
-                    LocalDate date = LocalDate.now();
-                    Period period = Period.between(resultSet.getDate("date").toLocalDate(), date);
-                    int updatePoints = resultSet.getInt("points") + points;
+                        readStatement.setInt(1, id);
+                        ResultSet resultSet = readStatement.executeQuery();
 
-                    if (period.toTotalMonths() * 100 - updatePoints >= 0) {
-                        PreparedStatement writeStatement = connection.prepareStatement(writeSQL);
-                        writeStatement.setInt(1, updatePoints);
-                        writeStatement.setInt(2, id);
-                        writeStatement.executeUpdate();
-                        System.out.println("\nС баланса члена №" + id + " списано " + points + " бонусов\n");
+                        if (resultSet.next()) {
+                            int points;
 
-                    } else System.out.println("\nОШИБКА: Бонусов не достаточно!\n");
+                            if (resultSet.getInt("clubid") == 0) {
+                                LocalDate date = LocalDate.now();
+                                Period period = Period.between(resultSet.getDate("date").toLocalDate(), date);
+                                points = (int) period.toTotalMonths() * 100 - resultSet.getInt("points");
+                                int updatePoints = resultSet.getInt("points") + bonus;
 
-                } else System.out.println("\nОШИБКА: Член не участвует в бонусной программе!\n");
+                                if (JOptionPane.showOptionDialog(null, "Имя члена: " + resultSet.getString("name") + "\nТип членства: " + resultSet.getString("type") + "\nНомер клуба: " + resultSet.getInt("clubid") + "\nЧленский взнос: " + resultSet.getInt("fees") + "\nБонусные баллы: " + points + "\nВсё верно?\nСПИСЫВАЕМ " + bonus + " БАЛЛОВ?!", "Информация члена ID №" + resultSet.getInt("id"), JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new Object[]{"да", "нет"}, "нет") == 0) {
 
-            } else System.out.println("\nОШИБКА: Члена с указанным номером нет в списках!\n");
+                                    if (period.toTotalMonths() * 100 - updatePoints >= 0) {
+                                        PreparedStatement writeStatement = connection.prepareStatement(writeSQL);
+                                        writeStatement.setInt(1, updatePoints);
+                                        writeStatement.setInt(2, id);
+                                        writeStatement.executeUpdate();
+                                        JOptionPane.showMessageDialog(null, "С баланса члена №" + id + " списано " + bonus + " баллов", "Списание бонусов члена ID №" + resultSet.getInt("id"), JOptionPane.INFORMATION_MESSAGE);
+                                    } else
+                                        JOptionPane.showMessageDialog(null, "ОШИБКА: Бонусных баллов недостаточно!", "Списание бонусов члена ID №" + resultSet.getInt("id"), JOptionPane.ERROR_MESSAGE);
+                                }
+                                ;
+                            } else
+                                JOptionPane.showMessageDialog(null, "ОШИБКА: Член не участвует в бонусной программе!", "Списание бонусов члена ID №" + resultSet.getInt("id"), JOptionPane.ERROR_MESSAGE);
 
-        } catch (SQLException | ClassNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
+                        } else
+                            JOptionPane.showMessageDialog(null, "ОШИБКА: Члена с указанным номером нет в списках!", "Списание бонусов члена ID №" + id, JOptionPane.ERROR_MESSAGE);
+
+                    } catch (SQLException | ClassNotFoundException f) {
+                        System.out.println(f.getMessage());
+                        JOptionPane.showMessageDialog(null, "ОШИБКА: Нет связи с базой данных!", "Списание бонусов члена ID №" + id, JOptionPane.ERROR_MESSAGE);
+                    }
+                } catch (Exception m) {
+                    System.out.println(m.getMessage());
+                    JOptionPane.showMessageDialog(null, "ОШИБКА: Введено не числовое значение!", "Списание бонусов члена ID №", JOptionPane.ERROR_MESSAGE);
+                }
+                ;
+            }
+        });
+
+        panel.add(buttonBonus);
+        JButton buttonBack = new JButton("НАЗАД");
+        buttonBack.addActionListener(e -> getChoice());
+
+        container.add(new Item());
+        container.add(new JLabel("СПИСАТЬ БОНУСЫ ЧЛЕНА №", SwingConstants.CENTER));
+        container.add(panel);
+        container.add(new JLabel("", SwingConstants.CENTER));
+        container.add(buttonBack);
+
+
+        frame.add(container);
+        frame.repaint();
+        frame.revalidate();
     }
-
 }
